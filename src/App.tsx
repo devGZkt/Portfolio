998561b4d3f7acba3f4f1101f9532bb69990bf55
Gabriel Zimmermann <gabriel.zimmermann.dev@gmail.com>
2024-09-20 11:33:05 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import CV from './components/CV';
 
 function App() {
   return (
     <>
-
+      <Navbar />
+      <Home />
+      <CV />
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="h-100vh">
+            <h1>Navabar</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
